46b7160f731274f0dfdde565294282348e021600
Dan Shen <dan.shen01@sap.com>
2017-09-18 16:23:52 +0800

    transform rule

--- src/util/faker.js
+++ src/util/faker.js
@@ -1,7 +1,7 @@
 import BookListData from '../mock/booklist.json'
 import BpData from '../mock/b1_bp_customer_data_20.json'
 import BpSimplifiedData from '../mock/b1_bp_customer_data_simplified_50.json'
-import BpMappingRule from '../mock/mappingRule2.json'
+import BpMappingRule from '../mock/mappingRule.json'
 
 export default {
     getBookListData() {
